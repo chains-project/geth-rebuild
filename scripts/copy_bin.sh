fbfe991e0298aac2fe1ddf1f765ea696fe009c3f
vivi365 <viviandersson0@gmail.com>
2024-07-04 18:01:06 +0200

    update copy script

--- scripts/copy_bin.sh
+++ scripts/copy_bin.sh
@@ -4,6 +4,8 @@ set -e
 
 DOCKER_TAG=$1
 OUTPUT_DIR=$2
+REF="geth-reference"
+REP="geth-reproduce"
 
 if [ -z "$DOCKER_TAG"  ] || [ -z "$OUTPUT_DIR" ]; then
   echo "Usage: $0 <docker tag> <output dir>"
@@ -12,14 +14,22 @@ fi
 
 mkdir -p "$OUTPUT_DIR"
 
+if [ -f "$OUTPUT_DIR/$REF" ]; then
+    rm "$OUTPUT_DIR/$REF"
+fi
+
+if [ -f "$REP" ]; then
+    rm "$REP"
+fi
+
 # run container in detached mode
 echo "Running container $DOCKER_TAG in detached mode."
 CONTAINER_ID=$(docker run -d "$DOCKER_TAG" /bin/sh) ||  { echo "failed to start container tagged $DOCKER_TAG"; exit 1; }
 
 # copy binaries and stop container
 echo "Copying binaries..."
-docker cp -q "$CONTAINER_ID":/bin/geth-reference "$OUTPUT_DIR"  || { echo "failed to copy /bin/geth-reference to $OUTPUT_DIR"; exit 1; }
-docker cp -q "$CONTAINER_ID":/bin/geth-reproduce "$OUTPUT_DIR" ||   { echo "failed to copy /bin/geth-reference to $OUTPUT_DIR"; exit 1; }
+docker cp -q "$CONTAINER_ID:/bin/$REF" "$OUTPUT_DIR"  || { echo "failed to copy /bin/geth-reference to $OUTPUT_DIR"; exit 1; }
+docker cp -q "$CONTAINER_ID:/bin/$REP" "$OUTPUT_DIR" ||   { echo "failed to copy /bin/geth-reference to $OUTPUT_DIR"; exit 1; }
 echo "Stopping container $CONTAINER_ID"
 docker stop "$CONTAINER_ID" || { echo "error: container id not found"; exit 1; }
 
