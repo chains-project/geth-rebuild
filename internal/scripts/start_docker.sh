3c2e5bb744ba04eaf9c5cebe7218a57621aac2a6
vivi365 <viviandersson0@gmail.com>
2024-08-05 13:20:50 +0200

    ubuntu compatible start docker script

--- internal/scripts/start_docker.sh
+++ internal/scripts/start_docker.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
 set -e
 
+OS=$(uname)
+
+if [ "$OS" = "Linux" ]; then
+    CMD="dockerd &"
+elif [ "$OS" = "Darwin" ]; then
+    # docker desktop...
+    CMD="open -a Docker"
+else
+    echo "Unsupported operating system: $OS"
+    exit 1
+fi
+
+
 if ! docker info > /dev/null 2>&1; then
     echo "Docker is not running. Opening Docker..."
-    open -a Docker || {
+    eval "$CMD" || {
         echo "Failed to start Docker."
         exit 1
     }
@@ -18,7 +31,7 @@ if ! docker info > /dev/null 2>&1; then
             echo "Timed out waiting for Docker to start."
             exit 1
         fi
-        sleep 5
+        sleep 4
     done
 fi
 echo "Docker is running."
