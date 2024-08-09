39e3ebbb3311100f99cfc7503480402b1547e6be
vivi365 <viviandersson0@gmail.com>
2024-08-09 23:31:40 +0200

    ...

--- cmd/experiments/main.go
+++ cmd/experiments/main.go
@@ -10,18 +10,16 @@ import (
 
 type ExperimentPaths struct {
 	RootDir     string
+	MainProgram string
 	Executable  string
 	CommitsFile string
 }
 
 var (
-	paths ExperimentPaths
-	exps  [][]experiments.ExperimentInput
-)
-var (
 	// All stable builds since 1.14.2, which is the upgrade from ubuntu bionic to noble in travis.yml
 	StableVersions = []string{"1.14.7", "1.14.6", "1.14.5", "1.14.4", "1.14.3", "1.14.2"}
 	Arches         = []utils.Arch{utils.AMD64, utils.A386, utils.ARM5, utils.ARM6, utils.ARM7, utils.ARM64}
+	paths          ExperimentPaths
 )
 
 func init() {
@@ -32,16 +30,23 @@ func init() {
 
 	paths = ExperimentPaths{
 		RootDir:     base,
+		MainProgram: filepath.Join(base, "cmd", "gethrebuild"),
 		Executable:  filepath.Join(base, "gethrebuild"),
 		CommitsFile: filepath.Join(base, "internal", "experiments", "data", "20_latest_commits.json")}
 
-	exps, err = experiments.GenerateAllExperiments(utils.Linux, Arches, StableVersions, paths.CommitsFile)
+	_, err = utils.RunCommand("go", "build", paths.MainProgram)
 	if err != nil {
-		log.Fatalf("error generating experiments: %v", err)
+		log.Fatalf("error creating executable: %v", err)
 	}
+
 }
 
 func main() {
+	exps, err := experiments.GenerateAllExperiments(utils.Linux, Arches, StableVersions, paths.CommitsFile)
+	if err != nil {
+		log.Fatalf("error generating experiments: %v", err)
+	}
+
 	for _, exp := range exps {
 		experiments.RunExperiments(exp, paths.Executable)
 	}
